17c9c6bf0f3d33dc3305b3bbb88ba2e18b7236e2
Mathew Dodds <mathewdodds@hotmail.com>
2023-03-15 19:05:38 -0300

    initial commit

---
+++ iles.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'iles'
+import windicss from 'vite-plugin-windicss'
+
+export default defineConfig({
+    vite: {
+        plugins: [
+          windicss(),
+        ],
+      }
+})
