ead6871290e148f4430f33a0bc40be87bd0d0c0d
Mathew Dodds <mathewdodds@hotmail.com>
2023-03-15 23:24:29 -0300

    fav

--- src/site.ts
+++ src/site.ts
@@ -1,4 +1,4 @@
 export default {
-  title: 'îles',
-  description: 'Islands of interactivity with Vue in Vite.js'
+  title: 'MPLAN Seminar Series 2023',
+  description: 'A series of discussions hosted by graduate planning students at Dalhousie University.'
 }
