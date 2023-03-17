39a30823fe44c78c0ae72b4952a5dd36592e8514
mathewdodds <50030480+mathewdodds@users.noreply.github.com>
2023-03-17 11:54:36 -0300

    Update event.vue

--- src/layouts/event.vue
+++ src/layouts/event.vue
@@ -24,7 +24,7 @@
                 </div>
                 <div class="mt-3 flex text-md text-pink-600 font-semibold mt-2 items-center">
                     <svg class="hidden sm:flex" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="17" height="17"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 20.9l4.95-4.95a7 7 0 1 0-9.9 0L12 20.9zm0 2.828l-6.364-6.364a9 9 0 1 1 12.728 0L12 23.728zM12 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 2a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"/></svg>
-                    <div class="ml-3">{{ $frontmatter.location }}</div>
+                    <div class="sm:ml-3">{{ $frontmatter.location }}</div>
                 </div>
                 <div class="flex text-sm mt-2">
                     <div>{{ $frontmatter.access }}</div>
